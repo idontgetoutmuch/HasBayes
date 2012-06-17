521f63c36b762045746c31b835c6118d5b706e2c
dominic <dominic@steinitz.org>
2012-06-17 16:17:58 +0000

    Start of the Metropolis Algorithm for a beta prior with a normal proposal distribution.

--- TestFu.hs
+++ TestFu.hs
@@ -35,11 +35,11 @@ rwalk count start gen =
   sampleRVarTWith MTL.lift $
   replicateM count rwalkState
 
-n = 10000
+n = 11112
 seed = 0
 m = 7
 
-proposedJumps :: [Bool]
+proposedJumps :: [Int]
 proposedJumps = map f $ fst $ runState (replicateM n (sampleRVar (uniform False True))) (mkStdGen seed)
   where f False = negate 1
         f True  = 1
@@ -58,8 +58,34 @@ f currentPosition (proposedJump, acceptOrReject) =
 p n | n >=1 && n <= m = n
     | otherwise       = 0
 
-main' = map (\j -> length $ filter (==j) $ drop (n `div` 10) $ scanl f 3 (zip proposedJumps' acceptOrRejects)) [1..m]
+main' = map (\j -> length $ filter (==j) $ drop (n `div` 10) $ scanl f 3 (zip proposedJumps acceptOrRejects)) [1..m]
 -- [323,655,915,1309,1627,1901,2271]
 
-likelihood :: Integer -> Integer -> Double -> Double
+likelihood :: Int -> Int -> Double -> Double
 likelihood z n theta = theta^z * (1 - theta)^(n - z)
+
+normals :: [Double]
+normals =  fst $ runState (replicateM n (sampleRVar (normal 0 0.1))) (mkStdGen seed)
+
+myData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0]
+
+q :: Double -> [Double] -> Double
+q position xs = likelihood z n position
+  where
+    n = length xs
+    z = length $ filter (== 1) xs
+
+g currentPosition (proposedJump, acceptOrReject) =
+  if acceptOrReject < probAccept
+    then
+      currentPosition + proposedJump
+    else
+      currentPosition
+  where
+    probAccept = min 1 (p (currentPosition + proposedJump) / p currentPosition)
+    p x | x < 0 = 0
+        | x > 1 = 0
+        | otherwise = f x
+    f = flip q myData
+
+main'' = drop (n `div` 10) $ scanl g 0.5 (zip normals acceptOrRejects)
